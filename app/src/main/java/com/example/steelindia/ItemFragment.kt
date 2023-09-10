27d324385ad2e0a8f95e8875cf74debc039ceeb1
GODRICC_13 <akshargoel@gmail.com>
2023-09-10 18:59:30 +0530

    hello

---
+++ app/src/main/java/com/example/steelindia/ItemFragment.kt
@@ -0,0 +1,30 @@
+package com.example.steelindia
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.steelindia.databinding.FragmentItemBinding
+
+class ItemFragment : Fragment() {
+
+
+
+    private lateinit var viewModel: ItemViewModel
+    private lateinit var binding: FragmentItemBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewModel = ViewModelProvider(this).get(ItemViewModel::class.java)
+        binding=FragmentItemBinding.inflate(layoutInflater)
+        return binding.root
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/steelindia/ItemViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.steelindia
+
+import androidx.lifecycle.ViewModel
+
+class ItemViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
